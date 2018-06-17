227cdb3a4cb72d76bbb8741f17c45b17706277a4
Michael Beckwith <tw2113@gmail.com>
2018-06-17 15:03:03 -0500

    start off some hooks that will filter in various languages for the current user.

--- inc/post-type-hooks.php
+++ inc/post-type-hooks.php
@@ -1,3 +1,14 @@
 <?php
 
 namespace tw2113\cptui18n;
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+function set_cptui_post_type_lang_for_user( $args, $post_type_slug, $post_type_args ) {
+
+	return $args;
+}
+add_filter( 'cptui_pre_register_post_type', __NAMESPACE__ . '\set_cptui_post_type_lang_for_user', 10, 3 );
--- inc/taxonomy-hooks.php
+++ inc/taxonomy-hooks.php
@@ -1,3 +1,15 @@
 <?php
 
 namespace tw2113\cptui18n;
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+
+function set_cptui_taxonomy_lang_for_user( $args, $taxonomy_slug, $taxonomy_args ) {
+
+	return $args;
+}
+add_filter( 'cptui_pre_register_taxonomy', __NAMESPACE__ . '\set_cptui_taxonomy_lang_for_user', 10, 3 );
