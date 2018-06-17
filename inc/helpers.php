c17c07f7fda7c499a8866748e8523338808169f6
Michael Beckwith <tw2113@gmail.com>
2018-06-17 15:01:55 -0500

    exit early and also add some notes for future development

--- inc/helpers.php
+++ inc/helpers.php
@@ -1,3 +1,14 @@
 <?php
 
 namespace tw2113\cptui18n;
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/*
+get_user_locale() // returns get_locale() default if none set for user.
+
+
+*/
