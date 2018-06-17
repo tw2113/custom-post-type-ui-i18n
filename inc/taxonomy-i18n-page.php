8d8d5ea626f5f6133d54c7ff67e34c18e289786e
Michael Beckwith <tw2113@gmail.com>
2018-06-17 15:00:49 -0500

    add our settings pages for language setting

---
+++ inc/taxonomy-i18n-page.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace tw2113\cptui18n;
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+function taxonomy_settings_page() {
+	?>
+	<div class="wrap cptui-i18n">
+		<h1 class="wp-heading-inline"><?php echo get_admin_page_title(); ?></h1>
+
+		dropdown to select language to use
+
+		fields to set each label, rewrite, etc
+	</div>
+	<?php
+}
